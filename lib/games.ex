c44450a70e235d041f2c4be47edc24b09b46e219
BLAXPIA05 <reaganw762@gmail.com>
2023-06-25 21:42:13 -0600

    finish making guessing game

--- lib/games.ex
+++ lib/games.ex
@@ -16,3 +16,15 @@ defmodule Games do
     :world
   end
 end
+
+defmodule Games.GuessingGame do
+  def play do
+    random_int = Enum.random(1..10)
+    int_parse = IO.gets("Guess a number betwees 1 and 10\n")
+    {int, _} = Integer.parse(int_parse)
+    cond do
+      int == random_int -> "You win!"
+      int != random_int-> "Incorrect!"
+    end
+  end
+end
